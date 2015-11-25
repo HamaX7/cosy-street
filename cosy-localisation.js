f89fdc254410d82b9ca56e3401260e1d3d91ae95
Stacie KITH <stacie.kith@gmail.com>
2015-11-25 23:08:28 +0100

    Changed to Nominatim API to get lat and lon

--- cosy-localisation.js
+++ cosy-localisation.js
@@ -1,10 +1,3 @@
-// Geolocalisation optionnelle
-
-//L.Control.geocoder().addTo(map);
-/*new L.Control.GeoSearch({
-provider: new L.GeoSearch.Provider.OpenStreetMap()
-}).addTo(map);*/
-
 function locateUser()
 {
     map.locate({setView: true, maxZoom: 17});
@@ -18,12 +11,37 @@ function locateUser()
 function locateAddress()
 {
     var adr = document.getElementById('get-address').value;
-    var geocoder = new L.Control.Geocoder.Google();
-    geocoder.geocode(adr, function(results) {
-        var latlng= new L.LatLng(results[0].center.lat, results[0].center.lng);
-        map.setView(latlng, 17);
-        L.marker(latlng).addTo(map);
-    });
 
-    console.log(adr);
+    // On lance la recherche avec nominatim pour recuperer toutes les infos sous format JSON
+    // On limite le resultat a 5 items (5 couples lng, lat)
+    var nominatimUrl = "http://nominatim.openstreetmap.org/search?format=json&limit=5&q=" + adr;
+    var getHttp = function()
+    {
+        this.get = function(url, callback)
+        {
+            var httpRequest = new XMLHttpRequest();
+            httpRequest.onreadystatechange = function()
+            {
+                if (httpRequest.readyState == 4 && httpRequest.status == 200)
+                    callback(httpRequest.responseText);
+            }
+            httpRequest.open("GET", url, true);
+            httpRequest.send();
+        }
+    }
+
+    var nominatimRequest = new getHttp();
+    nominatimRequest.get(nominatimUrl, function(response)
+    {
+        // Parsing du JSON
+        var locationInfo = JSON.parse(response);
+
+        // Zoom sur localisation et placement du marqueur
+        var latLon = new L.LatLng(locationInfo[0].lat, locationInfo[0].lon);
+        map.setView(latLon, 17);
+
+        var markerUser = L.marker(latLon);
+        markerUser.addTo(map);
+
+    });
 }
--- cosy.js
+++ cosy.js
@@ -1,12 +1,19 @@
-var map = L.map( 'map', {
-    center: [48.8534100, 2.3488000],
-    zoom: 15
-});
+var map;
 
-// Ajouter un layer
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: '<a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'cosystreet.o8f5hg5e',
-    accessToken: 'pk.eyJ1IjoiY29zeXN0cmVldCIsImEiOiJjaWhkaHRzN3UwYm9pdmJrbHZuaWIwOThrIn0.AjYuz32cxPVxwTlKpXG_Ww'
-}).addTo(map);
+function loadMap() {
+    map = L.map( 'map', {
+        center: [48.8534100, 2.3488000],
+        minZoom: 10,
+        zoom: 15
+    });
+
+    // Ajouter un layer
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: '<a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'cosystreet.o8f5hg5e',
+        accessToken: 'pk.eyJ1IjoiY29zeXN0cmVldCIsImEiOiJjaWhkaHRzN3UwYm9pdmJrbHZuaWIwOThrIn0.AjYuz32cxPVxwTlKpXG_Ww'
+    }).addTo(map);
+}
+
+window.onload = loadMap;
