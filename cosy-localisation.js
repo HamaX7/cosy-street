9337107f1d39d251120e97a4ec1a0f4966f21d7f
staciekith <stacie.kith@gmail.com>
2015-11-25 18:04:09 +0100

    Added localisation by address

--- cosy-localisation.js
+++ cosy-localisation.js
@@ -1,9 +1,9 @@
 // Geolocalisation optionnelle
 
 //L.Control.geocoder().addTo(map);
-new L.Control.GeoSearch({
+/*new L.Control.GeoSearch({
     provider: new L.GeoSearch.Provider.OpenStreetMap()
-}).addTo(map);
+}).addTo(map);*/
 
 function locateUser()
 {
@@ -18,7 +18,7 @@ function locateUser()
 function locateAddress()
 {
     var adr = document.getElementById('get-address').value;
-    var geocoder = new L.Control.Geocoder.Nominatim();
+    var geocoder = new L.Control.Geocoder.Google();
     geocoder.geocode(adr, function(results) {
            var latlng= new L.LatLng(results[0].center.lat, results[0].center.lng);
            map.setView(latlng, 17);
