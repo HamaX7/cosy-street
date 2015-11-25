1fe2280708612183abdc8bad7d5a592da3155b03
staciekith <stacie.kith@gmail.com>
2015-11-25 19:10:54 +0100

    Re-indentation

--- cosy-localisation.js
+++ cosy-localisation.js
@@ -2,7 +2,7 @@
 
 //L.Control.geocoder().addTo(map);
 /*new L.Control.GeoSearch({
-    provider: new L.GeoSearch.Provider.OpenStreetMap()
+provider: new L.GeoSearch.Provider.OpenStreetMap()
 }).addTo(map);*/
 
 function locateUser()
@@ -20,9 +20,9 @@ function locateAddress()
     var adr = document.getElementById('get-address').value;
     var geocoder = new L.Control.Geocoder.Google();
     geocoder.geocode(adr, function(results) {
-           var latlng= new L.LatLng(results[0].center.lat, results[0].center.lng);
-           map.setView(latlng, 17);
-           L.marker(latlng).addTo(map);
+        var latlng= new L.LatLng(results[0].center.lat, results[0].center.lng);
+        map.setView(latlng, 17);
+        L.marker(latlng).addTo(map);
     });
 
     console.log(adr);
