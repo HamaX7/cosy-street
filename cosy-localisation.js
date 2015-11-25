d0ce6f0f5f4d581502321b485db9cefbdf7b3544
staciekith <stacie.kith@gmail.com>
2015-11-25 17:02:06 +0100

    Added geosearch

--- cosy-localisation.js
+++ cosy-localisation.js
@@ -1,8 +1,13 @@
 // Geolocalisation optionnelle
 
+//L.Control.geocoder().addTo(map);
+new L.Control.GeoSearch({
+    provider: new L.GeoSearch.Provider.OpenStreetMap()
+}).addTo(map);
+
 function locateUser()
 {
-    map.locate({setView: true, maxZoom: 16});
+    map.locate({setView: true, maxZoom: 17});
     map.on('locationfound', onLocationFound);
 
     function onLocationFound(e) {
@@ -10,8 +15,15 @@ function locateUser()
     }
 }
 
-function locateAdress()
+function locateAddress()
 {
-    var adr = document.getElementById('get-adress').value;
+    var adr = document.getElementById('get-address').value;
+    var geocoder = new L.Control.Geocoder.Nominatim();
+    geocoder.geocode(adr, function(results) {
+           var latlng= new L.LatLng(results[0].center.lat, results[0].center.lng);
+           map.setView(latlng, 17);
+           L.marker(latlng).addTo(map);
+    });
+
     console.log(adr);
 }
